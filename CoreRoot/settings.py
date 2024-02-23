b8d19d01ee390a7fe22b090bbc0ba527bf058276
Vendigo <119477860+Vendifo@users.noreply.github.com>
2024-02-23 15:32:44 +0900

    Core

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -70,8 +70,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'CoreRoot.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+
 
 DATABASES = {
     'default': {
