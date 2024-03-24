97214dacd2d6fe9e8ee796518352afdfdeaf218b
Vendigo <119477860+Vendifo@users.noreply.github.com>
2024-03-25 04:37:17 +0900

    настройки для front

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -24,11 +24,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
 
+    'corsheaders',
+
     'core',
         'core.user',
         'core.auth',
         'core.post',
-        'core.comment'
+        'core.comment',
 ]
 
 MIDDLEWARE = [
@@ -39,6 +41,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'CoreRoot.urls'
@@ -123,3 +128,9 @@ REST_FRAMEWORK = {
         'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 15,
 }
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
